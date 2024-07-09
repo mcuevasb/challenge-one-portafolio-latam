d235c8683e8ebc2d6311ad19ab9deea74eb3a8a4
Manuel Cuevas <mcuevasb@gmail.com>
2024-07-09 09:15:56 -0600

    Updated 07-09-2024

--- validacion.js
+++ validacion.js
@@ -1,5 +1,16 @@
 //Haz tú validación en javascript acá
-function validaCURP(curp)
-{
-    return  /\w{4}\d{6}\w{3}$/.test(curp);
+function validaEmail(email){
+
+    if(email.includes("gmail"))
+    {
+        alert("Utiliza el correo de tu empresa");
+        return false;
+    }
+    
+    
+
+
+
+    
+
 }
